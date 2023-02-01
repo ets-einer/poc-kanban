c1629390589f6caa4f26956044208644d33d2077
iguoliveira <igor.or042@gmail.com>
2023-02-01 09:00:58 -0300

    add: Columns feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
-function App() {
+import { useId, useState } from "react"
+import { Columns } from "./components/columns"
+
+export const App = () => {
+  const [cols, setCols] = useState([{ id: Math.random().toString(16).slice(-4), title: 'Backlog' }])
+
+  function createNewCol() {
+    setCols([
+      ...cols,
+      {
+        id: 'col-' + Math.random().toString(16).slice(-4),
+        title: ''
+      }
+    ])
+  }
+
   return (
-    <div className="App">
+    <div className="flex gap-4">
+      {cols.map((item, index) => (
+        < Columns key={index} id={item.id} title={item.title} />
+      ))}
+      <button onClick={createNewCol}>Nova Coluna +</button>
     </div>
   )
 }
-export default App
