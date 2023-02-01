767ad104d976ec6a481dd999f8aa6835ca60d6ec
iguoliveira <igor.or042@gmail.com>
2023-02-01 09:23:17 -0300

    add: Colour attribute to cols

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { useId, useState } from "react"
+import { uniqueId } from "./utils/uniqueId"
 import { Columns } from "./components/columns"
 
 export const App = () => {
-  const [cols, setCols] = useState([{ id: Math.random().toString(16).slice(-4), title: 'Backlog' }])
+  const [cols, setCols] = useState([{ id: uniqueId("col-"), title: 'Backlog', colour: "#FAFAFA" }])
 
   function createNewCol() {
     setCols([
       ...cols,
       {
-        id: 'col-' + Math.random().toString(16).slice(-4),
-        title: ''
+        id: uniqueId("col-"),
+        title: "",
+        colour: ""
       }
     ])
   }
@@ -17,7 +19,7 @@ export const App = () => {
   return (
     <div className="flex gap-4">
       {cols.map((item, index) => (
-        < Columns key={index} id={item.id} title={item.title} />
+        < Columns key={index} id={item.id} title={item.title} colour={item.colour} />
       ))}
       <button onClick={createNewCol}>Nova Coluna +</button>
     </div>
