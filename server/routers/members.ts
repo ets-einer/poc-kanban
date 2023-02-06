706a4a96a0f2218e9b301054f2d33a6d160afc30
icarodr <it46009@gmail.com>
2023-02-06 00:30:02 -0300

    add new member router

--- server/routers/members.ts
+++ server/routers/members.ts
@@ -5,6 +5,25 @@ import { publicProcedure, router } from '../utils/trpc';
 
 //Terminate Member Routes
 
+const createMemberSchema = z.object({
+    nameMember: z.string(),
+    emailMember: z.string(),
+    edvMember: z.string()
+});
+
 export const memberRouter = router({
-    
-})
+    allMembers: publicProcedure.query(async () => {
+        try {
+            const allMembers = await prisma.member.findMany();
+            return { allMembers }
+        } catch (error) {
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                cause: error,
+                message: 'Failed to get all Members'
+            })
+        }
+    }),
+
+    //Create new edit data mutation
+});
